2d84a49dc052a5c0b1c55ef64346a94503ca94f4
Victor nyoyoko <vfranktor@gmail.com>
2023-01-17 13:51:18 +0000

    fix(__init__.py): fix notify() not speaking goals & include shutdown for skill

--- __init__.py
+++ __init__.py
@@ -1,7 +1,7 @@
 import asyncio
 import os
 import json 
-from datetime import datetime
+from datetime import datetime as dt
 from mycroft import MycroftSkill, intent_handler
 from notion_client import Client, APIErrorCode, APIResponseError
 
@@ -26,18 +26,18 @@ class NotionRoutine(MycroftSkill):
         self.log.info("initializing connection to notion...")
         self.database_page = []
         self.routine_dictionary = {} #Dictionary of time as KEY and goal as VALUE
-        self.time_list = []
+        self.time_list = [] #Time objects for comparison
 
         access_token = NOTION_SETTINGS['access_token']
         # access_token = self.settings.get('access_token')
         os.environ['NOTION_TOKEN'] = access_token
-        self.notion = Client(auth=os.environ['NOTION_TOKEN']) 
+        self.notion = Client(auth=os.environ['NOTION_TOKEN'])
+        self.running = True
         self.load_routine()
-        self.map_routine()
+        # self.map_routine()
 
-
-        
     def load_routine(self):
+        """ Loads routine from notion database"""
         try:
             # database_id = self.settings.get('database_id')
             database_id = NOTION_SETTINGS['database_id']
@@ -46,19 +46,17 @@ class NotionRoutine(MycroftSkill):
                 **{
                 "database_id": database_id,
                 })
-            # await asyncio.sleep(delay)
 
-            # self.log.info(self.database_page)
             self.log.info("connected to notion database...")
             self.map_routine()
-            self.key_to_datetime()
+            self.add_to_time_list()
+            # self.log.info(self.time_list)
             self.log.info("loaded database content")
-            # self.notification_manager()
-            # self.log.info(type) 
-            asyncio.run(self.notification_manager())
+            asyncio.run(self.notification_manager(self.running))
         except APIResponseError as error:
             if error.code == APIErrorCode.ObjectNotFound:
                 self.log.exception("could not connnect to database")
+            self.log.exception(error)
 
 
     def map_routine(self):
@@ -81,29 +79,34 @@ class NotionRoutine(MycroftSkill):
             #     # TODO raise exception data not received from Notion
 
 
-    def key_to_datetime(self):
-            """ converts time key into datetime object"""
-            for key in self.routine_dictionary.keys():
-                self.time_list.append(str(datetime.strptime(key, "%I:%M%p").time()))
+    def add_to_time_list(self):
+        """ converts formatted time to string and appends to time list"""
+        for key in list(self.routine_dictionary.keys()):
+            self.time_list.append(str(dt.strptime(key, "%I:%M%p").time()))
+            # self.log.info(type(self.time_list))
 
-    def datetime_to_key(self, time):
-        """ converts datetime object to string(time key) in routine_dict"""
-        return datetime.strptime(time, "%H:%M:%S").time().strftime("%-I:%M%p")
+    def convert_to_index(self, time):
+        """ converts datetime object to a formatted string for indexing in routine_dict"""
+        return dt.strptime(time, "%H:%M:%S").time().strftime("%-I:%M%p")
 
-    async def notification_manager(self):
-        """ Handles the removal of time from time_list and notifying user of goal"""
+    async def notification_manager(self, running):
+        """ Handles the removal of time from time_list and when to notify user"""
 
-        # self.log.info(type(self.timer()))
-        while True:
+        while running:
             #optimise by popping from list while retreiving from database to make it pythonic
             time = await self.timer()
-            goal = self.routine_dictionary[self.datetime_to_key(time)]
-            self.log.info(time)
-            self.log.info(goal)
+            goal = self.routine_dictionary[self.convert_to_index(time)]
+            # self.log.info(time)
+            # self.log.info(goal)
             self.notify(time, goal)
 
             if time in self.time_list:
+                self.log.info("removing time from time_list")
                 self.time_list.remove(time)
+
+            # now = dt.now()
+            # current_time = now.strftime("%H:%M")
+            # if 
 
   
     async def timer(self):
@@ -111,7 +114,7 @@ class NotionRoutine(MycroftSkill):
         self.log.info("timer started")
         while True:
 
-            now = datetime.now()
+            now = dt.now()
             current_hour = now.strftime("%H")
             current_min = now.strftime("%M")
 
@@ -122,16 +125,24 @@ class NotionRoutine(MycroftSkill):
 
     def notify(self, time, goal):
         """ Notifies user of goal"""
-        self.speak_dialog('notify', {'time': time}, {'goal': goal})
+        self.speak_dialog('notify', {'time': time, 'goal': goal})
 
     @intent_handler('routine.notion.intent')
     def handle_routine_notion(self, message):
         """ Handles the routine.notion intent"""
         self.speak_dialog('routine.notion')
 
+    def update_routine(self):
+        """ reloads routine from notion database every hour"""
+        self.load_routine()
+
 
     def stop(self):
         pass
+
+    def shutdown(self):
+        """ Stop the skill"""
+        self.running = False
 
 def create_skill():
     """ Create skill and return it to the loader"""
